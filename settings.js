aabc14737997d69a651568b6a78a7d6ad74e5d6d
Tzafrir Rehan <tzafrir.r@gmail.com>
2012-08-04 22:02:21 +0300

    Separate post numbering settings for pages

--- settings.js
+++ settings.js
@@ -1,15 +1,21 @@
 (function() {
+  function complicatedDefaults(str) {
+    var defaults = {
+      promoText: 'shares',
+      alternateAccount: '0'
+    }
+    if (str.indexOf('postNumbering') == 0) {
+      return '0';
+    }
+    return defaults[str];
+  }
+
   function _getName(name) {
     return '_setting_' + name;
   };
-  var defaults = {
-    promoText: 'shares',
-    alternateAccount: '0',
-    postNumbering: '0'
-  }
   window.Settings = {
     get: function(name) {
-      return localStorage[_getName(name)] || defaults[name] || '';
+      return localStorage[_getName(name)] || complicatedDefaults(name) || '';
     },
     set: function(name, value) {
       localStorage[_getName(name)] = value;
